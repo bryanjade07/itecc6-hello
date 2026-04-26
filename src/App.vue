c8caa89c1f16b063b28740220d955f2a5fb611b8
Jade Bryan Corpuz <jadebryancorpuz7@gmail.com>
2026-04-26 14:10:13 +0800

    feat: updated greeting and configured supabase

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template> 
+  <div> class="app">
+  <h1> Hello, I'm Jade Bryan Corpuz... </h1>
+  <p> Welcome to my Vue.js project </p>
+  </div>
+</template>
